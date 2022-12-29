35c9a27572bdc22bddb315db8c94d31e0f5493fc
Shelton Ngwenya <shelt15.nn@gmail.com>
2022-12-29 20:59:45 +0000

    fixed some imp mistakes

---
+++ node_messages.py
@@ -0,0 +1,6 @@
+# @author Shelton Ngwenya, R00203947
+
+class NodeMessages:  # enum class, has all possible node messages
+    HELLO = "hello"
+    TYPE_GRADIENT = "gradient"
+    TYPE_DATA = "data"
