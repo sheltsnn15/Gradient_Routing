abe6db18121346f97e873807f2d02f69932487a9
Shelton Ngwenya <shelt15.nn@gmail.com>
2022-12-30 07:51:43 +0000

    small changes

--- routing_proto.py
+++ routing_proto.py
@@ -1,104 +1,10 @@
 import wsnsimpy.wsnsimpy as wsp
+
 from node_messages import NodeMessages
 
 INFINITY = float('inf')
 TRICKLE_MIN_PERIOD = 0.1
 TRICKLE_MAX_PERIOD = 2.0
-
-
-# class RoutingProtocol:
-#
-#     def __init__(self, node):
-#         super().__init__(node.sim, node.id, node.pos)
-#         self.trickle_max_period = TRICKLE_MAX_PERIOD
-#         self.trickle_period = TRICKLE_MIN_PERIOD
-#         self.node = node
-#         self.best_distance = INFINITY
-#         self.rank = INFINITY
-#         self.preferred_parent = None
-#         self.potential_parents = {}
-#         self.trickle_timer = self.create_event()
-#         self.phy = wsp.DefaultPhyLayer(self)
-#
-#     def send_gradient_update(self):
-#         pdu = {
-#             "type": "gradient",
-#             "sender": self.id,
-#             "rank": self.rank,
-#             "best_distance": self.best_distance,
-#         }
-#         self.send(wsp.BROADCAST_ADDR, pdu=pdu)
-#
-#     def on_receive(self, sender, pdu):
-#         if pdu["type"] == "gradient":
-#             self.process_gradient_update(sender, pdu)
-#         elif pdu["type"] == "data":
-#             self.process_data_packet(sender, pdu)
-#
-#     def process_gradient_update(self, sender, pdu):
-#         # Update list of potential parents
-#         self.potential_parents[sender] = {
-#             "rank": pdu["rank"],
-#             "best_distance": pdu["best_distance"],
-#             "last_seen": self.node.now,
-#         }
-#
-#         # Update preferred parent if necessary
-#         if (
-#                 self.preferred_parent is None
-#                 or pdu["rank"] + 1 < self.rank
-#                 or (pdu["rank"] + 1 == self.rank and pdu["best_distance"] < self.best_distance)
-#         ):
-#             self.rank = pdu["rank"] + 1
-#             self.best_distance = pdu["best_distance"]
-#             self.preferred_parent = sender
-#
-#     def process_data_packet(self, sender, pdu):
-#         if self.id == 0:  # Network sink
-#             self.log(f"Received data packet from node {sender}")
-#         else:
-#             self.send(self.preferred_parent, pdu=pdu)
-#
-#     def remove_parent(self, parent_id):
-#         del self.potential_parents[parent_id]
-#
-#         if parent_id == self.preferred_parent:
-#             self.preferred_parent = None
-#             self.rank = INFINITY
-#
-#             # Find new preferred parent from list of potential parents
-#             if self.potential_parents:
-#                 self.preferred_parent = min(
-#                     self.potential_parents,
-#                     key=lambda p: (self.potential_parents[p]["rank"], self.potential_parents[p]["best_distance"]),
-#                 )
-#                 self.rank = self.potential_parents[self.preferred_parent]["rank"] + 1
-#
-#     def send_pdu(self, pdu):
-#         if self.preferred_parent is None:
-#             return False
-#         else:
-#             self.send(self.preferred_parent, pdu=pdu)
-#             return True
-#
-#     def run(self):
-#         # Start Trickle timer
-#         self.trickle_timer = self.create_event()
-#         self.delayed_exec(TRICKLE_MIN_PERIOD, self.trickle_timer.succeed)
-#         while True:
-#             yield self.trickle_timer
-#             self.send_gradient_update()
-#             if self.trickle_interval < TRICKLE_MAX_PERIOD:
-#                 self.trickle_interval *= 2
-#             self.trickle_timer = self.create_event()
-#             self.delayed_exec(self.trickle_interval, self.trickle_timer.succeed)
-#
-#     def monitor_parents(self):
-#         while True:
-#             yield self.timeout(2 * TRICKLE_MAX_PERIOD)
-#             for parent_id, parent_info in list(self.potential_parents.items()):
-#                 if self.now - parent_info["last_seen"] > 2 * TRICKLE_MAX_PERIOD:
-#                     self.remove_parent(parent_id)
 
 
 class RoutingProtocol:
@@ -169,18 +75,23 @@ class RoutingProtocol:
                     self.preferred_parent = self.node.sim.get_node(parent["id"])
                     self.best_distance = parent["best_distance"]
                     self.rank = self.best_distance
+            # If there are no more potential parents, set the trickle timer interval to the maximum value
             if not self.preferred_parent:
                 self.trickle_timer.interval = TRICKLE_MAX_PERIOD
+            # Otherwise, set the trickle timer interval to the minimum value and reset the timer
             else:
                 self.trickle_timer.interval = TRICKLE_MIN_PERIOD
                 self.trickle_timer.reset()
 
     def send_pdu(self, pdu):
+        # Check if the node has a preferred parent
         if self.preferred_parent is None:
             return False
+        # Set the type of the PDU to "data" and the sender ID to the ID of the node
         pdu.type = "data"
         pdu.sender_id = self.node.id
 
+        # Create a new PDU containing the data message and send it to the preferred parent
         pdu = wsp.PDU(
             None,
             len(NodeMessages.TYPE_DATA) * 8,
@@ -193,7 +104,10 @@ class RoutingProtocol:
         return True
 
     def on_receive_pdu(self, pdu):
+        # Handle a received PDU depending on its type
         if pdu.type == "gradient":
+            # Update the preferred parent, best distance, and rank of the node based on the information
+            # in the PDU
             if pdu.rank < self.rank:
                 self.update_parent(pdu.sender_id)
             elif pdu.rank == self.rank:
@@ -202,12 +116,14 @@ class RoutingProtocol:
                 elif pdu.best_distance == self.best_distance:
                     if pdu.sender_id < self.node.id:
                         self.update_parent(pdu.sender_id)
+            # Add the sender of the PDU to the list of potential parents
             self.potential_parents[pdu.sender_id] = {
                 "id": pdu.sender_id,
                 "rank": pdu.rank,
                 "best_distance": pdu.best_distance,
             }
         elif pdu.type == "data":
+            # Forward the data message to the preferred parent if the node has one, otherwise drop the message
             if self.node.id == 0:
                 self.node.on_receive_pdu(pdu)
             else:
